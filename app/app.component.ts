1c0f13a843d20e6c741d283123c6e442ecdd2718
Jason Downing <jhdowning@gmail.com>
2016-10-21 09:22:01 -0700

    create initial file structure along with accompanying gulp and bower files

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Meal Time</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+  public masterMealList: Meal[] = [
+      new Meal("poulet bonne femme", "delicious", 450),
+      new Meal("cafe au lait", "ole!", 90),
+      new Meal("bouillabaisse", "dabest", 622),
+      new Meal("baguette", "the crunchy kind", 200)
+  ];
+}
